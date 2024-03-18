4fe1dc55512314ec27859ad38ec994f4565409e3
Harshita Vemula <47696280+HarshitaVemula@users.noreply.github.com>
2024-03-18 12:48:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -26,66 +26,67 @@ st.markdown(
 )
 
 # Get your openai api key
-openai_url = "https://platform.openai.com/api-keys"
-st.caption("[Get your OpenAI API key.](%s)" % openai_url)
-api_key = st.text_input(
-    "Login and click on create new key. Input the API key generated.", type="password"
-)
-os.environ["OPENAI_API_KEY"] = api_key
-api_key_valid = is_api_key_valid(api_key)
+# openai_url = "https://platform.openai.com/api-keys"
+# st.caption("[Get your OpenAI API key.](%s)" % openai_url)
+# api_key = st.text_input(
+#     "Login and click on create new key. Input the API key generated.", type="password"
+# )
+# os.environ["OPENAI_API_KEY"] = api_key
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+# api_key_valid = is_api_key_valid(api_key)
 
 col1, col2 = st.columns(2)
 
-if api_key_valid:
-    from prompts import itin_chain, coord_chain
-    from templates import itinerary_config, coordinates_config, validation_config
+# if api_key_valid:
+from prompts import itin_chain, coord_chain
+from templates import itinerary_config, coordinates_config, validation_config
 
-    # Initialize
+# Initialize
+with col1:
+    st.write("Where do you want to travel next?")
+    st.write(
+        '<span style="color:grey">Do include how many days you want to travel for and under what budget, let me help you! An example prompt can look like \n "Travel to sf for 2 days under 300\$"</span>',
+        unsafe_allow_html=True,
+    )
+
+    user_input = st.text_input("")
+
+output = itin_chain({"query": user_input})
+
+# col1
+if user_input:
     with col1:
-        st.write("Where do you want to travel next?")
-        st.write(
-            '<span style="color:grey">Do include how many days you want to travel for and under what budget, let me help you! An example prompt can look like \n "Travel to sf for 2 days under 300\$"</span>',
-            unsafe_allow_html=True,
+        st.write(f"Great! Let's plan your trip!.")
+        output[itinerary_config.chain_output_key] = output[
+            itinerary_config.chain_output_key
+        ].replace("$", "\$")
+        st.write(output[itinerary_config.chain_output_key])
+    is_valid = json.loads(output[validation_config.chain_output_key])[
+        validation_config.system_prompt_output_key
+    ]
+
+    if is_valid == str(1):
+        coord = coord_chain(
+            {
+                itinerary_config.chain_output_key: output[
+                    itinerary_config.chain_output_key
+                ]
+            }
         )
+        coord = json.loads(coord[coordinates_config.system_prompt_output_key])
+        coord = coord[coordinates_config.chain_output_key]
 
-        user_input = st.text_input("")
+        locations = list(coord.keys())
 
-    output = itin_chain({"query": user_input})
+        m = folium.Map(location=coord[locations[0]], zoom_start=12)
+        for k in locations[1:20]:
+            folium.Marker(coord[k], popup=k, tooltip=k).add_to(m)
 
-    # col1
-    if user_input:
-        with col1:
-            st.write(f"Great! Let's plan your trip!.")
-            output[itinerary_config.chain_output_key] = output[
-                itinerary_config.chain_output_key
-            ].replace("$", "\$")
-            st.write(output[itinerary_config.chain_output_key])
-        is_valid = json.loads(output[validation_config.chain_output_key])[
-            validation_config.system_prompt_output_key
-        ]
-
-        if is_valid == str(1):
-            coord = coord_chain(
-                {
-                    itinerary_config.chain_output_key: output[
-                        itinerary_config.chain_output_key
-                    ]
-                }
-            )
-            coord = json.loads(coord[coordinates_config.system_prompt_output_key])
-            coord = coord[coordinates_config.chain_output_key]
-
-            locations = list(coord.keys())
-
-            m = folium.Map(location=coord[locations[0]], zoom_start=12)
-            for k in locations[1:20]:
-                folium.Marker(coord[k], popup=k, tooltip=k).add_to(m)
-
-            with col2:
-                st.text("")
-                st.text("")
-                st.text("")
-                st.text("")
-                st.text("")
-                st.text("")
-                map_data = st_folium(m, width=725, returned_objects=[])
+        with col2:
+            st.text("")
+            st.text("")
+            st.text("")
+            st.text("")
+            st.text("")
+            st.text("")
+            map_data = st_folium(m, width=725, returned_objects=[])
